93efbb9853a02f823af6315cfaa16c4e6dd3c8f5
ChristophLasinger <56270498+ChristophLasinger@users.noreply.github.com>
2021-10-04 13:42:38 +0200

    Add project files.

---
+++ MyQuery.ConApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyQuery.Logic;
+using System;
+
+namespace MyQuery.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intList = new int[] { 1, 2, 3 };
+            var doubleList = intList.Map(i => Convert.ToDouble(i));
+
+
+        }
+    }
+}
---
+++ MyQuery.Logic/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyQuery.Logic
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<TResult> Map<T, TResult> (this IEnumerable<T> source, Func<T, TResult> mapping)
+        {
+            source.CheckArgument(nameof(source));
+            mapping.CheckArgument(nameof(mapping));
+
+            var result = new List<TResult>();
+
+            foreach (var item in source)
+            {
+                result.Add(mapping(item));
+            }
+            return result;
+        }
+    }
+}
